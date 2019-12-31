eb6b1c4c2fb6870ffa3fd62d27479015240191ae
fenqiangy <fenqiangy@tujia.com>
2020-01-01 00:55:09 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  "title": "striveYan",
+  "description": "strive blog",
+  "dest": "public",
+  // "base": "striveyan",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "关于我",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          },
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    // "friendLink": [
+    //   {
+    //     "title": "午后南杂",
+    //     "desc": "Enjoy when you can, and endure when you must.",
+    //     "email": "1156743527@qq.com",
+    //     "link": "https://www.recoluan.com"
+    //   },
+    //   {
+    //     "title": "vuepress-theme-reco",
+    //     "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+    //     "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+    //     "link": "https://vuepress-theme-reco.recoluan.com"
+    //   }
+    // ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "striveYan",
+    "authorAvatar": "/avatar.png",
+    // "record": "xxxx",
+    "startYear": "2019"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
